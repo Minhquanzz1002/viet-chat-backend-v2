aba3e02f0db94ddb9dcb85f59ce96a8de1b34716
Nguyễn Minh Quân <quannguyenminh1001@gmail.com>
2024-01-23 07:54:43 +0700

    #1 config security

---
+++ src/main/java/vn/edu/iuh/advisers/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package vn.edu.iuh.advisers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
---
+++ src/main/java/vn/edu/iuh/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package vn.edu.iuh.config;
+
+import jakarta.servlet.FilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    public static final String[] AUTH_WHITELIST = {
+            "/actuator/**",
+            "/swagger-ui/**",
+            "/v1/auth/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .authorizeHttpRequests(authorize -> {
+                    authorize
+                            .requestMatchers(AUTH_WHITELIST).permitAll()
+                            .anyRequest().authenticated();
+                });
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);                                // default: null
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));  // default: null
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));          // default: null
+        configuration.setAllowedHeaders(Collections.singletonList("*"));        // default: null
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
