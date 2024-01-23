21516ac7c460d31c43a84eb1bca9e570ef428ea5
Nguyễn Minh Quân <quannguyenminh1001@gmail.com>
2024-01-23 07:18:04 +0700

    init project

---
+++ src/main/java/vn/edu/iuh/VietChatApiApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietChatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VietChatApiApplication.class, args);
+    }
+
+}
